ec83ea89535991aa63dfe5727855a73560b197fc
Igor V. Kruglenko <igor.kruglenko@gmail.com>
2018-02-21 03:17:36 +0200

    Add React.JS with stub from create-react-app, add production mode, many fixed dependenses, add yarn

--- src/main.js
+++ src/main.js
@@ -1,25 +1,67 @@
-// Шаблон делегирования
-// Делегирование (англ. Delegation) — основной шаблон проектирования, 
-// в котором объект внешне выражает некоторое поведение, но в реальности 
-// передаёт ответственность за выполнение этого поведения связанному объекту. 
-// Шаблон делегирования является фундаментальной абстракцией, на основе 
-// которой реализованы другие шаблоны - композиция (также называемая агрегацией), 
-// примеси (mixins) и аспекты (aspects).
+import 'React' from 'react';
+import 'ReactDOM' from 'react-dom';
 
 
-// Класс B имеет метод-заглушку (method stub), который 
-// передаёт методы foo() и bar() классу A. Класс B делает вид, что он имеет атрибуты класса A.
-
-
-class Foobar {
-	foo(){
-		console.log('Класс Foobar: метод foo()');
-	}
-	bar(){
-		console.log('Класс Foobar: метод bar()');
-	}
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
 }
 
-class beekeeper{
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
 
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
 }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,99 @@
-const path = require('path');
+global.Promise = require ('bluebird');
+
+const webpack = require('webpack'); // Доступ к встроенным плагинам
+const path = require('path');       //
+const dist = path.resolve(__dirname, 'public');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const dist = path.resolve(__dirname, 'public');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-module.exports = {
-        entry: './src/main.js',
-        devtool: 'inline-source-map',
-        plugins: [
-            new UglifyJsPlugin(),
-            new CleanWebpackPlugin([dist]),
-            new HtmlWebpackPlugin({
-                title: 'Hot Module Replacement',
-                filename: 'index.html',
-                template: './src/assets/index.html'
-            })
-        ],
-        module: {
-            rules: [{
-                    test: /\.(png|svg|jpg|gif)$/,
-                    use: ['file-loader']
-                }, 
-                {
-                    test: /\.(scss|sass)$/,
-                    use: ['style-loader', 'css-loader', 'sass-loader']
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    },
-                }]
+
+const cssName            = process.env.NODE_ENV === 'production' ? 'styles-[hash].css' : 'styles.css';
+const jsName             = process.env.NODE_ENV === 'production' ? 'bundle-[hash].js' : 'bundle.js';
+
+const plugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            BROWSER:  JSON.stringify(true),
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+        }
+    }),
+    new ExtractTextPlugin(cssName),
+        new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement',
+        filename: 'index.html',
+        template: './src/assets/index.html'
+    }),
+    new webpack.LoaderOptionsPlugin({
+       debug: true
+     })
+];
+
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(
+    new CleanWebpackPlugin([ 'assets/' ], {
+        root: dist,
+        verbose: true,
+        dry: false
+    })
+    );
+    // plugins.push(new webpack.optimize.DedupePlugin());
+    // plugins.push(new webpack.optimize.OccurenceOrderPlugin());
+    plugins.push(new UglifyJsPlugin());
+
+}
+
+const config = {
+    // Точка входа
+    entry: ['babel-polyfill', './src/index.js'],
+    // debug: process.env.NODE_ENV !== 'production',
+    resolve: {
+        // root:               path.join(__dirname, 'src'),
+        // modulesDirectories: ['node_modules'],
+        extensions:         ['.js', '.jsx']
+    },
+    devtool: process.env.NODE_ENV !== 'production' ? 'inline-source-map' : false,
+
+    plugins,
+
+    // Собираемый бандл
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, dist)
+    },
+
+    // Модули
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }, 
+            {
+                test: /\.(scss|sass|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
-            output: {
-                filename: 'js/bundle.js',
-                path: path.resolve(__dirname, dist)
-            },
-            devServer: {
-                compress: true,
-                port: 9000
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ["@babel/preset-env","@babel/preset-react"]
+                }
             }
-        };
+        }]
+    },
+    devServer: {
+        compress: true,
+        port: 9000,
+        headers: { 'Access-Control-Allow-Origin': '*' }
+    },
+
+
+};
+
+
+module.exports = config;
